06532ed96ca8527193ee9fd2f88f7e3aafaa32f6
Luqman Ali <luqmanalihrp@gmail.com>
2024-05-05 04:37:32 +0500

    Logout Added

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -30,7 +30,15 @@
                     <button class="themebtn-trans">
                             DASHBOARD
                     </button>
-                </a>
+            </a>
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                    <a>
+                        <button type="submit" class="themebtn-trans">
+                                Logout
+                        </button>
+                    </a>
+            </form>
             @endauth
         </div>
         <!-- Side Bar -->
@@ -73,6 +81,14 @@
                             DASHBOARD
                     </button>
                 </a>
+                <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                    <a>
+                        <button type="submit" class="themebtn-trans">
+                                Logout
+                        </button>
+                    </a>
+                </form>
             @endauth
             </div>
 
@@ -80,65 +96,3 @@
         <!-- Side Bar -->
     </div>
 </Header>
-
-{{-- <div class="csgo-upperHeader-container">
-    <a href="/"><img src="/assets/logo.svg" alt="" style="height: 100px; width:100px;"></a>
-</div>
-<div class="main-site-inner-section">
-    <div class="csgo-header-container">
-        <!-- <div class="left">logo</div> -->
-
-        <div class="addLocationProduct-dropdown">
-            <div class="addLocationProduct-toggle" onclick="addLocationProducttoggleDropdown()">
-                <img src="/assets/img/CSGO.png" alt="">
-                CS2 <span class="addLocationProduct-arrow"></span>
-            </div>
-            <div class="addLocationProduct-dropdown-menu">
-                <p class="addLocationProduct-option" href="#">CS2</p>
-                <p class="addLocationProduct-option" href="#">Valorant</p>
-                <p class="addLocationProduct-option" href="#">Call Of Duty</p>
-            </div>
-        </div>
-
-
-        <div class="csgo-menu-conatiner">
-            <ul>
-                <li><a href="/csgoboost">CS2 Boost</a></li>
-                <li><a href="/faceit">Faceit Boost</a></li>
-                <li><a href="/esea">Esea Boost</a></li>
-                <li><a href="/esportal">Esportal Boost</a></li>
-                <li><a href="/coaching">Coaching</a></li>
-                <li><a href="/about">About</a></li>
-                @guest
-                <li class="hidden-links" style="padding: 1%;"><a href="/login" class="csgo-btn csgo-login-btn">Login</a></li>
-                <li class="hidden-links"><a href="/register">Register</a></li>
-                @endguest
-                @auth
-                <li class="hidden-links"><a href="/dashboard">Dashboard</a></li>
-                @endauth
-                
-            </ul>
-        </div>
-        <div>
-            @guest
-            <a href="/login"><button class="csgo-btn csgo-login-btn">Login</button></a>
-            <a href="/register"><button class="csgo-btn csgo-login-btn">Register</button></a>
-            @endguest
-            @auth
-            <a href="/dashboard"><button class="csgo-btn csgo-login-btn">Dashboard</button></a>
-            
-                <a onclick="document.getElementById('logout').submit();"><button class="csgo-login-btn">Logout</button></a>
-                <form action="/logout" method="post" id="logout">
-                    @csrf
-                </form>
-            @endauth
-        </div>
-        
-        <div class="sidebar-header">
-            <div class="burger-line"></div>
-            <div class="burger-line"></div>
-            <div class="burger-line"></div>
-        </div>
-        <!--  -->
-    </div>
-</div> --}}
