61c8e0e76f5cd92128ef9709920e19d129e75358
Luqman Ali <luqmanalihrp@gmail.com>
2024-05-06 05:01:08 +0500

    Footer fixed, removed subscription link

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -45,13 +45,11 @@
 
     <div class="footer-right-container">
         <div class="footersubscribe-container">
-            <h2>Subscribe To Our Newsletter</h2>
+            <h2>Register Now!</h2>
             <div class="flex setDirection-input gap-4 mt-5">
-                <div class="footersubscribe-input-container flex">
-                    <img src="/assets/icons/✳️ Start Icon.png" alt="">
-                    <input type="text" placeholder="Enter Your Email Here">
-                </div>
-                <button class="themebtn-bg">Subscribe</button>
+                <a href="/register"><button class="themebtn-bg">Create An Account!</button></a>
+                <a href="/login"><button class="themebtn-bg">Login</button></a>
+                <a href="/csboost"><button class="themebtn-bg">Boost Account</button></a>
             </div>
         </div>
         <div class="flex justify-end">
@@ -59,55 +57,3 @@
         </div>
     </div>
 </Footer>
-
-{{-- 
-<input type="range" min="0" max="10" value="2" class="slider-range"
-     id="myRange" style="display: none;"></input>
-     
-     <div class="csgo-HomeFooter-container" style="text-align: center;">
-    <img src="/assets/logo.svg" alt="" class="footer-logo" style="height: 100px; width:100px;">    
-    <div class="csgo-Home-InnerFooter-container">
-        <div class="csgo-Home-InnerFooter-section">
-            <h5>CONTACT US</h5>
-            <div class="footer-line-span"></div>
-            <ul class="csgo-footer-list-container">
-                <li><a href="#"><img src="/assets/icons/facebook.svg" alt="">Facebook</a></li>
-                <li><a href="#"><img src="/assets/icons/twitch.svg" alt="">Twitch</a></li>
-                <li><a href="#"><img src="/assets/icons/twitter.svg" alt="">Twitter</a></li>
-                <li><a href="#"><img src="/assets/icons/mail.svg" alt="">Mail Us</a></li>
-            </ul>
-        </div>
-        <div class="csgo-Home-InnerFooter-section">
-            <h5>SERVICES</h5>
-            <div class="footer-line-span"></div>
-            <ul class="csgo-footer-list-container">
-                <li><a href="/csgoboost">CS2 Boosting</a></li>
-                <li><a href="/faceit">Faceit Boosting</a></li>
-                <li><a href="/esea">Esea Boosting</a></li>
-                <li><a href="/esportal">Esportal Boosting</a></li>
-            </ul>
-        </div>
-        <div class="csgo-Home-InnerFooter-section">
-            <h5>HELP</h5>
-            <div class="footer-line-span"></div>
-            <ul class="csgo-footer-list-container">
-                <li><a href="/contact">Contact Us</a></li>
-                <li><a href="/jobs">Jobs</a></li>
-            </ul>
-        </div>
-        <div class="csgo-Home-InnerFooter-section">
-            <h5>Important Links</h5>
-            <div class="footer-line-span"></div>
-            <ul class="csgo-footer-list-container">
-                <li><a href="/faqs">FAQs</a></li>
-                <li><a href="/privacy">Privacy Policy</a></li>
-                <li><a href="/terms">Terms & Conditions</a></li>
-            </ul>
-        </div>
-    </div>
-</div>
-<div class="lowerfooter">
-    <h6>2023 © THEMEOWL.NET. All rights reserved.</h6>
-    <p>Counter-Strike: Global Offensive is a registered trademark of Valve Corporation Company We are in no way
-        affiliated with, associated with or endorsed by Valve Corporation.</p>
-</div> --}}
