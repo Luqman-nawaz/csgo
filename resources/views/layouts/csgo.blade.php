58edd7d04f521d490a3dd06f441ec231e4991b4e
Luqman Ali <luqmanalihrp@gmail.com>
2024-06-26 14:20:36 +0500

    Added GA

--- resources/views/layouts/csgo.blade.php
+++ resources/views/layouts/csgo.blade.php
@@ -26,6 +26,16 @@
 
         <link rel="icon" type="image/x-icon" href="/assets/fav.png">
 
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-RKC8P5P1WQ"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-RKC8P5P1WQ');
+        </script>
+
         @yield('title')
     </head>
 
