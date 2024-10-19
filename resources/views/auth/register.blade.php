587f784adc8a736342150304d725e7fc1c2c85dd
Luqman Ali <luqmanalihrp@gmail.com>
2024-10-19 19:53:44 +0500

    Added responsive social buttons to register page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,7 +1,16 @@
 @extends('layouts.csgo')
 @section('title') <title>MyBoost.GG - Register Now!</title> @endsection
 
-    
+    @push('css')
+        <style>
+            @media (max-width: 650px) {
+                .SocialBtn{
+                    font-size: 0;
+                }
+            }
+        </style>
+    @endpush
+
     @section('content')
 
         
@@ -19,12 +28,12 @@
                         <div class="w-full flex justify-between items-start">
                             <h2>Create Account</h2>
                             <div class="flex gap-3 mt-3">
-                                <a href="{{ url('auth/google') }}"><button class="themebtn-bg w-full mt-5">
+                                <a href="{{ url('auth/google') }}"><button class="themebtn-bg SocialBtn w-full mt-5">
                                     <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="18" height="18" viewBox="0 0 30 30">
                                         <path d="M 15.003906 3 C 8.3749062 3 3 8.373 3 15 C 3 21.627 8.3749062 27 15.003906 27 C 25.013906 27 27.269078 17.707 26.330078 13 L 25 13 L 22.732422 13 L 15 13 L 15 17 L 22.738281 17 C 21.848702 20.448251 18.725955 23 15 23 C 10.582 23 7 19.418 7 15 C 7 10.582 10.582 7 15 7 C 17.009 7 18.839141 7.74575 20.244141 8.96875 L 23.085938 6.1289062 C 20.951937 4.1849063 18.116906 3 15.003906 3 z"></path>
                                     </svg>
                                 &nbsp; Google</button></a>
-                                <a href="{{ url('auth/twitter') }}"><button class="themebtn-bg w-full mt-5">
+                                <a href="{{ url('auth/twitter') }}"><button class="themebtn-bg SocialBtn w-full mt-5">
                                     <img src="https://upload.wikimedia.org/wikipedia/commons/5/53/X_logo_2023_original.svg" alt="" style="width:18px; height:18px;">
                                 &nbsp; Twitter</button></a>
                                 {{-- <a href="{{ url('auth/google') }}"><button class="themebtn-bg w-full mt-5">
