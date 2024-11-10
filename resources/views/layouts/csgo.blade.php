5fd33770795580711f7690059d46446e61115ab6
Luqman Ali <luqmanalihrp@gmail.com>
2024-11-10 17:31:29 +0500

    Meta pixel added

--- resources/views/layouts/csgo.blade.php
+++ resources/views/layouts/csgo.blade.php
@@ -7,6 +7,23 @@
         <link rel="icon" href="/assets/Logo.svg" type="image/svg+xml">
         <link rel="stylesheet" href="/vendor/css/index.css">
         <link rel="stylesheet" href="/vendor/css/style.css">
+        <!-- Meta Pixel Code -->
+        <script>
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '1586682518637956');
+        fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=1586682518637956&ev=PageView&noscript=1"
+        /></noscript>
+        <!-- End Meta Pixel Code -->
         <!-- slider -->
         @stack('css')
         <style>
